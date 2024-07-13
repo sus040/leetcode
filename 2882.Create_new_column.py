adf0f00de4d83dd694ef50c25833544c24d9ebc0
sus040 <144386075+sus040@users.noreply.github.com>
2024-07-13 15:47:08 -0400

    Delete 2882.Create_new_column.py

--- 2882.Create_new_column.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    # Add a column of bonus that contains the doubled values of salary
-    employees['bonus']= employees['salary'] * 2
-    # Return the data frame
-    return employees
