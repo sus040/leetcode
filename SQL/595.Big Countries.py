4875544cf1776db325d7daf92e8c4e592b2231bd
songfronitsky <songsuu43@gmail.com>
2024-07-15 11:34:11 -0400

    update

---
+++ SQL/595.Big Countries.py
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# Select name, population, area columns only
+Select name, population, area
+# From world table 
+From world
+# Filter where area is at least 3000000 or population of at least twenty-five million.
+where area >= 3000000 or population >= 25000000;
